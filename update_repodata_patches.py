74cbe1f34a0beaa1a00a2740b4e25f8fb3ac35ec
Matthew R. Becker <becker.mr@gmail.com>
2021-11-17 15:51:40 -0600

    ENH cronjob for repodata patching

---
+++ update_repodata_patches.py
@@ -0,0 +1,93 @@
+import sys
+import subprocess
+import os
+import tempfile
+import github
+import datetime
+
+
+def _commit_to_patches(tmpdir):
+    subprocess.check_call(
+        "git reset --hard HEAD",
+        cwd=os.path.join(tmpdir, "conda-forge-repodata-patches-feedstock"),
+        shell=True,
+    )
+
+    subprocess.check_call(
+        "git commit --allow-empty -am 'resync repo data for weekly cron-job'",
+        cwd=os.path.join(tmpdir, "conda-forge-repodata-patches-feedstock"),
+        shell=True,
+    )
+
+    subprocess.check_call(
+        "git push",
+        cwd=os.path.join(tmpdir, "conda-forge-repodata-patches-feedstock"),
+        shell=True,
+    )
+
+
+def _post_issue_with_diff(diff):
+    msg = """\
+Hi! Our weekly job found a non-zero repodata patch diff:
+
+<details>
+
+```
+%s
+```
+
+</details>
+""" % diff
+
+    dstr = datetime.date.today().strftime("%Y-%m-%d")
+    gh = github.Github(os.environ['GITHUB_TOKEN'])
+    repo = gh.get_repo("conda-forge/conda-forge-repodata-patches-feedstock")
+    repo.create_issue(
+        "[%s] non-zero repodata patch diff" % dstr,
+        body=msg,
+    )
+
+
+def update_repodata_patches(dry_run):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        subprocess.check_call(
+            "git clone https://github.com/conda-forge/"
+            "conda-forge-repodata-patches-feedstock.git",
+            cwd=tmpdir,
+            shell=True,
+        )
+
+        subprocess.check_call(
+            "git remote set-url --push origin "
+            "https://x-access-token:${GITHUB_TOKEN}@github.com/conda-forge/"
+            "conda-forge-repodata-patches-feedstock.git",
+            cwd=os.path.join(tmpdir, "conda-forge-repodata-patches-feedstock"),
+            shell=True,
+        )
+
+        d = subprocess.check_output(
+            "python show_diff.py",
+            cwd=os.path.join(
+                tmpdir,
+                "conda-forge-repodata-patches-feedstock",
+                "recipe"
+            ),
+            shell=True,
+        ).decode("utf-8")
+
+        if len(d) > 0:
+            print("diff:\n" + d, flush=True)
+            if not dry_run:
+                _post_issue_with_diff(d)
+                _commit_to_patches(tmpdir)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        raise RuntimeError("Need 0 or 1 arguments")
+    if len(sys.argv) == 2 and sys.argv[1] == '--dry-run':
+        dry_run = True
+    else:
+        dry_run = False
+
+    update_repodata_patches(dry_run)
