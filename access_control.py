91efc6f10e40ced15d71682fd9759246ad773543
Amit Kumar <dtu.amit@gmail.com>
2023-09-07 05:25:30 +0000

    add typehints

--- access_control.py
+++ access_control.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 import requests
 from ruamel.yaml import YAML
+from typing import List, Dict
 
 GH_ORG = os.environ.get("GH_ORG", "conda-forge")
 
@@ -36,7 +37,7 @@ CIRUN_FILENAME_RESOURCE_MAPPING = {
 ACCESS_YAML_FILENAME = ".access_control.yml"
 
 
-def _get_access_control_files(path):
+def _get_access_control_files(path: str) -> List[str]:
     access_path = Path(path)
     all_files = list(access_path.glob("*"))
     return (
@@ -44,7 +45,7 @@ def _get_access_control_files(path):
     )
 
 
-def _get_filename_feedstock_mapping(path):
+def _get_filename_feedstock_mapping(path: str) -> Dict[str, str]:
     access_files = _get_access_control_files(path)
     file_name_feedstock_mapping = {}
     for file_path in access_files:
@@ -56,7 +57,7 @@ def _get_filename_feedstock_mapping(path):
     return file_name_feedstock_mapping
 
 
-def _process_access_control_requests(path, remove=True):
+def _process_access_control_requests(path: str, remove: bool = True) -> None:
     print(f"Processing access control requests for {path}")
     grant_access_request = _get_filename_feedstock_mapping(path)
     for filename, feedstocks in grant_access_request.items():
@@ -74,14 +75,16 @@ def _process_access_control_requests(path, remove=True):
                 update_access_yaml(resource, feedstock_repo, action=action)
 
 
-def process_access_control_requests():
+def process_access_control_requests() -> None:
     """Process access control requests"""
     print("Processing access control request")
     _process_access_control_requests("grant_access", remove=False)
     _process_access_control_requests("revoke_access")
 
 
-def _process_request_for_feedstock(feedstock, resource, remove, policy_args):
+def _process_request_for_feedstock(
+        feedstock: str, resource: str, remove: bool, policy_args: List[str]
+) -> None:
     feedstock_clone_path = tempfile.mkdtemp()
     assert GH_ORG
     clone_cmd = f"git clone --depth 1 https://github.com/{GH_ORG}/{feedstock}.git {feedstock_clone_path}"
@@ -118,7 +121,7 @@ def _process_request_for_feedstock(feedstock, resource, remove, policy_args):
     )
 
 
-def check_if_repo_exists(feedstock_name):
+def check_if_repo_exists(feedstock_name: str) -> None:
     repo = f"{feedstock_name}-feedstock"
     owner_repo = f"{GH_ORG}/{repo}"
     print(f"Checking if {owner_repo} exists")
@@ -126,7 +129,7 @@ def check_if_repo_exists(feedstock_name):
     if response.status_code != 200:
         raise ValueError(f"Repository: {owner_repo} not found!")
 
-def _check_for_path(path):
+def _check_for_path(path: str) -> None:
     mapping = _get_filename_feedstock_mapping(path)
     for filename, feedstocks in mapping.items():
         print(f"Checking if {filename} is in {CIRUN_FILENAME_RESOURCE_MAPPING.keys()}")
@@ -136,12 +139,12 @@ def _check_for_path(path):
     if not mapping:
         print(f"Nothing to check for: {path}")
 
-def check():
+def check() -> None:
     """Check requests are valid (resources exist, feedstocks exist)"""
     _check_for_path("grant_access")
     _check_for_path("revoke_access")
 
-def _commit_after_files_removal(push=True):
+def _commit_after_files_removal(push: bool = True) -> None:
     subprocess.check_call(
         "git add .",
         shell=True,
@@ -158,7 +161,7 @@ def _commit_after_files_removal(push=True):
         )
 
 
-def _remove_input_files(dir, file_to_keep):
+def _remove_input_files(dir: str, file_to_keep: str) -> None:
     print("Removing input files")
     directory = Path(dir)
     files_to_keep = [Path(file_to_keep)]
@@ -172,7 +175,9 @@ def _remove_input_files(dir, file_to_keep):
     _commit_after_files_removal(push=False)
 
 
-def update_access_yaml(resource, feedstock_name, action, filename=ACCESS_YAML_FILENAME):
+def update_access_yaml(
+        resource: str, feedstock_name: str, action: str, filename: str = ACCESS_YAML_FILENAME
+) -> None:
     """
     Manage feedstock in .access_control.yml.
 
@@ -219,7 +224,7 @@ def update_access_yaml(resource, feedstock_name, action, filename=ACCESS_YAML_FI
 
 
 
-def main():
+def main() -> None:
     """Process requests"""
     check()  # doesn't hurt
     process_access_control_requests()
