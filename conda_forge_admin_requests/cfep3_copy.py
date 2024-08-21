3ce2a936a99fb0b28f06521148c20d72788f6b9b
jaimergp <jaimergp@users.noreply.github.com>
2024-08-21 18:37:32 +0200

    use list of dicts for clarity

--- conda_forge_admin_requests/cfep3_copy.py
+++ conda_forge_admin_requests/cfep3_copy.py
@@ -19,12 +19,12 @@ def check_one(package: str, sha256: str):
             f"Key '{sha256}' must be SHA256 for the artifact (64 hexadecimal characters)"
         )
     
-    channel, subdir, artifact = package.rsplit("/", 3)
-    channel, _ = split_label_from_channel(channel)
+    channel_and_maybe_label, subdir, artifact = package.rsplit("/", 2)
+    channel, _ = split_label_from_channel(channel_and_maybe_label)
     pkg_name, version, _, _ = parse_filename(artifact)
 
     # Check existence
-    url = f"https://conda-web.anaconda.org/{channel}/{subdir}/{artifact}"
+    url = f"https://conda-web.anaconda.org/{channel_and_maybe_label}/{subdir}/{artifact}"
     r = requests.head(url)
     if not r.ok:
         raise ValueError(f"Package '{package}' at {channel} does not seem to exist")
@@ -44,12 +44,18 @@ def check_one(package: str, sha256: str):
 
 def check(request: Dict[str, Any]) -> None:
     packages = request.get("anaconda_org_packages")
-    if not packages or not isinstance(packages, dict):
+    if not packages or not isinstance(packages[0], dict):
         raise ValueError(
-            "Must define 'anaconda_org_packages' as a dict of [sha256, owner/subdir/artifact]"
+            "Must define 'anaconda_org_packages' as a list of dicts with keys "
+            "{'package': channel/subdir/artifact, 'sha256': SHA256}"
     )
-    for sha256, package in packages.items():
-        check_one(package, sha256)
+    for item in packages:
+        if not item.get("package") or not item.get("sha256"):
+            raise ValueError(
+                "Each 'anaconda_org_packages' entry must be a dict with keys "
+                "{'package': channel/subdir/artifact, 'sha256': SHA256}"
+            ) 
+        check_one(item["package"], item["sha256"])
 
 
 def run(request: Dict[str, Any]) -> Dict[str, Any] | None:
@@ -60,10 +66,10 @@ def run(request: Dict[str, Any]) -> Dict[str, Any] | None:
     if to_label:
         to_label = ("--to-label", to_label)
     packages_to_try_again = []
-    for sha256, package in request["anaconda_org_packages"].items():
-        check_one(package, sha256)
-        channel, subdir, artifact = package.rsplit("/", 3)
-        channel, label = split_label_from_channel(channel)
+    for item in request["anaconda_org_packages"]:
+        check_one(item["package"], item["sha256"])
+        channel_and_maybe_label, subdir, artifact = item["package"].rsplit("/", 2)
+        channel, label = split_label_from_channel(channel_and_maybe_label)
         from_label = ("--from-label", label) if label != "main" else ()
         pkg_name, version, _, _ = parse_filename(artifact)
         spec = f"{channel}/{pkg_name}/{version}/{subdir}/{artifact}"
@@ -78,13 +84,13 @@ def run(request: Dict[str, Any]) -> Dict[str, Any] | None:
             *to_label,
             spec
         ]
-        print("Copying", package, "...")
+        print("Copying", item["package"], "...")
         p = subprocess.run(cmd)
         if p.returncode == 0:
             print("... OK!")
         else:
             print("... failed!")
-            packages_to_try_again.append(package)
+            packages_to_try_again.append(item)
 
     
     if packages_to_try_again:
