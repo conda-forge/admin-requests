e4f5d51421975096ea46403dc7899a420949ccf0
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-01-25 22:27:35 -0600

    fix: correct exception (#1348)

--- conda_forge_admin_requests/feedstock_outputs.py
+++ conda_forge_admin_requests/feedstock_outputs.py
@@ -8,6 +8,14 @@ from conda_forge_metadata.feedstock_outputs import sharded_path as _get_sharded_
 import github
 
 
+def _test_and_raise_besides_file_not_exists(e: github.GithubException):
+    if isinstance(e, github.UnknownObjectException):
+        return
+    if e.status == 404 and "No object found" in e.data["message"]:
+        return
+    raise e
+
+
 def _add_feedstock_output(
     feedstock,
     pkg_name,
@@ -17,7 +25,8 @@ def _add_feedstock_output(
     repo = gh.get_repo("conda-forge/feedstock-outputs")
     try:
         contents = repo.get_contents(_get_sharded_path(pkg_name))
-    except github.UnknownObjectException:
+    except github.GithubException as e:
+        _test_and_raise_besides_file_not_exists(e)
         contents = None
 
     if contents is None:
